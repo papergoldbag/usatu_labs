f65cd6ec3418c7f868113e93787a7adeda24c4a8
Arsen <56455347+papergoldbag@users.noreply.github.com>
2021-09-13 03:47:48 +0500

    res

--- 2/main.cpp
+++ 2/main.cpp
@@ -7,6 +7,10 @@
 using namespace std;
 
 
+const int NUMS_SIZE = 15;
+const int MICROSEC_SLEEP_TIME = 600;
+
+
 enum ConsoleColor
 {
         Black         = 0,
@@ -55,7 +59,7 @@ void print_color_array(int *nums, int size, int i_past, int i_new){
         if (i == i_past){
             set_color(Green);
         }else if (i == i_new){
-            set_color(Blue);
+            set_color(Yellow);
         }
         printf("%d ", nums[i]);
         set_color(White);
@@ -87,13 +91,13 @@ int bubble_sort(int *nums, int size){
                 c += 1;
                 clear_last_line_console();
                 print_color_array(nums, size, j, j+1);
+                printf("  | %d", c);
                 swapped = true;
-                sleep_micro(600);
+                sleep_micro(MICROSEC_SLEEP_TIME);
             }
         }
         if (!swapped)break;
     }
-    printf("\n");
     return c;
 }
 
@@ -107,36 +111,57 @@ int isertion_sort(int *nums, int size){
                 c += 1;
                 clear_last_line_console();
                 print_color_array(nums, size, j-1, j);
-                sleep_micro(600);
+                printf("  | %d", c);
+                sleep_micro(MICROSEC_SLEEP_TIME);
             }else break;
         }
     }
-    printf("\n");
     return c;
 }
 
 
-int quick_sort(int *nums, int size){
-    int i = 0;
-    int j = 0;
-    int left = 0,
-    int right = 0;
-    int middle = 0;
+int quick_sort_c = 0;
+int quick_sort(int *nums, int size, int from_i, int to_i)
+{
+    int i = from_i;
+    int j = to_i;
+    int middle = nums[(i + j) / 2];
+
+    while (i <= j)
+    {
+        while (nums[i] < middle)i++;
+        while (nums[j] > middle)j--;
+        if (i <= j)
+        {
+            swap(&nums[i], &nums[j]);
+            quick_sort_c++;
+            clear_last_line_console();
+            print_color_array(nums, size, j-1, j);
+            printf("  | %d", quick_sort_c);
+            sleep_micro(MICROSEC_SLEEP_TIME);
+            i++;
+            j--;
+        }
+    }
+    if (j > from_i)
+        quick_sort(nums, size, from_i, j);
+    if (i < to_i)
+        quick_sort(nums, size, i, to_i);
+
+    return quick_sort_c;
 }
-
-
-const int NUMS_SIZE = 15;
 
 
 int main(){
     setlocale(LC_ALL,"RUSSIAN");
     set_color(White);
+    srand(time(0));
 
     int nums[NUMS_SIZE] = {};
     for (int i = 0; i < NUMS_SIZE; i++){
-        nums[i] = rand() % 1000;
+        nums[i] = (rand()+1000) % 1000;
     }
-    printf("Rand nums: ");
+    printf("Rand %d nums: ", NUMS_SIZE);
     print_array(nums, NUMS_SIZE);
 
     int sort_num = 0;
@@ -145,16 +170,22 @@ int main(){
     int c_operations = 0;
     switch (sort_num) {
         case 1:
+            printf("starting bubble sort...\n");
             c_operations = bubble_sort(nums, NUMS_SIZE);
             break;
         case 2:
+            printf("starting isertion sort...\n");
             c_operations = isertion_sort(nums, NUMS_SIZE);
             break;
+        case 3:
+            printf("starting quick sort...\n");
+            c_operations = quick_sort(nums, NUMS_SIZE, 0, NUMS_SIZE-1);
     }
+    printf("\n");
     clear_last_line_console();
-    printf("Operations amount is %d\n", c_operations);
     printf("Sorted nums: ");
     print_array(nums, NUMS_SIZE);
+    printf("Operations amount is %d\n", c_operations);
 
     system("pause");
     return 0;
---
+++ push.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m res
+git push origin master
