a3bd0007d2ee09b435aea6849602452232dd0113
arsen <sabarsenrash@gmail.com>
2021-12-22 01:30:11 +0500

    res

--- 6/main.c
+++ 6/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
+#include <time.h>
 
 
 double* create_array(int size){
@@ -38,23 +38,26 @@ void input_rand_matrix(double** matrix, int strings, int columns){
   double tmp = 0;
   for (int i = 0; i < strings; i++){
     for (int j = 0; j < columns; j++){
-      matrix[i][j] = (double)(rand() % 100);
+      matrix[i][j] = (double)(rand() % 10);
     }
   }
 }
 
 
 void print_matrix(double** matrix, int strings, int columns){
+  double t = 0;
   for (int i = 0; i < strings; i++){
     for (int j = 0; j < columns; j++){
-      printf("%.2lf\t", matrix[i][j]);
+      t = matrix[i][j];
+      if ((int)t == 0) t = 0;
+      printf("%.2lf\t", t);
     }
     printf("\n");
   }
 }
 
 
-double determinant(double **matrix, int size) {
+double det(double **matrix, int size) {
     double temp, determinant;
     temp = 0;
     determinant = 1; // to plus
@@ -142,14 +145,35 @@ double **multiply_matrixes(double **matrix_a, double **matrix_b, int a_strings, 
 }
 
 
+double ** minus_matrixes(double **matrix_a, double **matrix_b, int strings, int columns){
+  double ** matrix = create_matrix(strings, columns);
+  for (int i = 0; i<strings; i++){
+    for (int j = 0; j<strings; j++){
+      matrix[i][j] = matrix_a[i][j] - matrix_b[i][j];
+    }
+  }
+  return matrix;
+}
+
+
+double ** sum_matrixes(double **matrix_a, double **matrix_b, int strings, int columns){
+  double ** matrix = create_matrix(strings, columns);
+  for (int i = 0; i<strings; i++){
+    for (int j = 0; j<strings; j++){
+      matrix[i][j] = matrix_a[i][j] + matrix_b[i][j];
+    }
+  }
+  return matrix;
+}
+
+
 int main(){
   //matrix A
-  double **matrix_a;
   int matrix_a_strings, matrix_a_columns, matrix_a_input_mode;
   printf("Введите размерность матрицы А:");
   scanf("%d", &matrix_a_strings);
   matrix_a_columns = matrix_a_strings;
-  matrix_a = create_matrix(matrix_a_strings, matrix_a_columns);
+  double **matrix_a = create_matrix(matrix_a_strings, matrix_a_columns);
   printf("Задать значения элементов матрицы А вручную (1) или случайным образом (2)?");
   scanf("%d", &matrix_a_input_mode);
   if (matrix_a_input_mode == 1){
@@ -160,13 +184,13 @@ int main(){
 
   printf("Матрица A:\n");
   print_matrix(matrix_a, matrix_a_strings, matrix_a_columns);
+  printf("\n");
 
   // Matrix B
-  double **matrix_b;
   int matrix_b_columns, matrix_b_input_mode;
   printf("Введите количество столбцов матрицы В:");
   scanf("%d", &matrix_b_columns);
-  matrix_b = create_matrix(matrix_a_strings, matrix_b_columns);
+  double **matrix_b = create_matrix(matrix_a_strings, matrix_b_columns);
   printf("Задать значения элементов матрицы B вручную (1) или случайным образом (2)?");
   scanf("%d", &matrix_b_input_mode);
   if (matrix_b_input_mode == 1){
@@ -179,7 +203,7 @@ int main(){
   printf("\n");
 
   // det(A)
-  double det_a = determinant(matrix_a, matrix_a_strings);
+  double det_a = det(matrix_a, matrix_a_strings);
   printf("det(A)=%.2lf", det_a);
   printf("\n\n");
 
@@ -190,10 +214,38 @@ int main(){
   printf("\n");
 
   // X=A(^-1)*B
-  double **ab = multiply_matrixes(invers_matrix_a, matrix_b, matrix_a_strings, matrix_a_strings, matrix_a_strings, matrix_b_columns);
+  double **matrix_x = multiply_matrixes(invers_matrix_a, matrix_b, matrix_a_strings, matrix_a_strings, matrix_a_strings, matrix_b_columns);
   printf("X=(A^-1)*B\n");
   printf("X:\n");
-  print_matrix(ab, matrix_a_strings, matrix_b_columns);
+  print_matrix(matrix_x, matrix_a_strings, matrix_b_columns);
+  printf("\n");
+
+  // check
+  printf("Проверка\n");
+
+  // A * X
+  double ** matrix_ax = multiply_matrixes(matrix_a, matrix_x, matrix_a_strings, matrix_a_strings, matrix_a_strings, matrix_b_columns);
+  printf("A*X:\n");
+  print_matrix(matrix_ax, matrix_a_strings, matrix_b_columns);
+  printf("\n");
+
+  // A*X-B
+  double ** matrix_ax_minus_b = minus_matrixes(matrix_ax, matrix_b, matrix_a_strings, matrix_b_columns);
+  printf("A*X-B:\n");
+  print_matrix(matrix_ax_minus_b, matrix_a_strings, matrix_a_strings);
+  printf("\n");
+
+  // A*(A^-1)
+  double ** matrix_a_mul_inv_a = multiply_matrixes(matrix_a, invers_matrix_a, matrix_a_strings, matrix_a_strings, matrix_a_strings, matrix_a_strings);
+  printf("A*(A^-1):\n");
+  print_matrix(matrix_a_mul_inv_a, matrix_a_strings, matrix_a_strings);
+  printf("\n");
+
+  // (A^-1)*A
+  double ** matrix_inv_a_mul_a = multiply_matrixes(invers_matrix_a, matrix_a, matrix_a_strings, matrix_a_strings, matrix_a_strings, matrix_a_strings);
+  printf("(A^-1)*A:\n");
+  print_matrix(matrix_inv_a_mul_a, matrix_a_strings, matrix_a_strings);
+  printf("\n");
 
   printf("\n");
   return 0;
