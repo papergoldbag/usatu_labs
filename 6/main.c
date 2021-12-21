477e3c6247f0cabee587d5f9b522220eb5fe8296
arsen <sabarsenrash@gmail.com>
2021-12-22 00:01:42 +0500

    res

--- 6/main.c
+++ 6/main.c
@@ -51,7 +51,36 @@ void print_matrix(double** matrix, int strings, int columns){
 
 
 int main(){
-  
+  //matrix A
+  double **matrix_a;
+  int matrix_a_strings, matrix_a_columns, matrix_a_input_mode;
+  printf("Введите размерность матрицы А:");
+  scanf("%lf", &matrix_a_strings);
+  matrix_a_columns = matrix_a_strings;
+
+  printf("Задать значения элементов матрицы А вручную (1) или случайным образом (2)?");
+  scanf("%d", &matrix_a_input_mode);
+  if (matrix_a_input_mode == 1){
+    input_matrix(matrix_a, matrix_a_strings, matrix_a_columns);
+  }else{
+    input_rand_matrix(matrix_a, matrix_a_strings, matrix_a_columns);
+  }
+
+//   Введите размерность матрицы А: ->n
+//   Задать значения элементов матрицы А вручную (1) или случайным образом (2)? -
+//   >1/2
+//   Введите количество столбцов матрицы В: ->mЗадать значения элементов матрицы B вручную (1) или случайным образом (2)? -
+//   >1/2
+//   A=... B=...
+//   det(A)=...
+//   A^-1=...
+//   X=A^-1*B
+//   X=...
+//   Проверка:
+//   A*X=...
+//   A*X-B=...
+//   A*A^-1=...
+//   A^-1*A=...
 
   return 0;
 }
